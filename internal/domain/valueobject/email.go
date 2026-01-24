801d63bf286523030faf8cdacd6c251eafa15aac
Seigha Rufus <excellenceseigha@gmail.com>
2026-01-24 16:36:41 +0100

    feat: implement email value object

---
+++ internal/domain/valueobject/email.go
@@ -0,0 +1,42 @@
+package valueobject
+
+import (
+	"regexp"
+	"strings"
+
+	domainErrors "github.com/Ruseigha/LabukaAuth/internal/domain/errors"
+)
+
+type Email struct {
+	value string
+}
+
+func NewEmail(email string) (Email, error) {
+	
+		if strings.TrimSpace(email) == "" {
+			return Email{}, domainErrors.ErrEmptyEmail
+		}
+
+		emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
+		if !emailRegex.MatchString(email) {
+        return Email{}, domainErrors.ErrInvalidEmailFormat
+    }
+
+		normalizedEmail := strings.ToLower(strings.TrimSpace(email))
+
+		return Email{value: normalizedEmail}, nil
+}
+
+
+func (e Email) String() string {
+    return e.value
+}
+
+func (e Email) Equals(other Email) bool {
+    return e.value == other.value
+}
+
+
+func (e Email) IsEmpty() bool {
+    return e.value == ""
+}
