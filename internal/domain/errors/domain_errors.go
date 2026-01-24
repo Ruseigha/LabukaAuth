368cc484b3d3f09bb09a03460df37f39b39b2518
Seigha Rufus <excellenceseigha@gmail.com>
2026-01-24 16:15:32 +0100

    feat: add domain errors

---
+++ internal/domain/errors/domain_errors.go
@@ -0,0 +1,11 @@
+package errors
+
+import "errors"
+
+var (
+	ErrInvalidEmailFormat = errors.New("invalid email format")
+	ErrWeakPassword = errors.New("password does not meet complexity requirements")
+	ErrEmptyUserId = errors.New("user ID cannot be empty")
+	ErrEmptyEmail = errors.New("email cannot be empty")
+	ErrPasswordHashMissing = errors.New("password hash is missing")
+)
