5bed68b392ac23083e1c414482c0a66e1dc5af11
Seigha Rufus <excellenceseigha@gmail.com>
2026-01-24 22:30:51 +0100

    create user repository

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,17 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/Ruseigha/LabukaAuth/internal/domain/entity"
+	"github.com/Ruseigha/LabukaAuth/internal/domain/valueobject"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user *entity.User) error
+	FindByID(ctx context.Context, id valueobject.UserID) (*entity.User, error)
+	FindByEmail(ctx context.Context, email valueobject.Email) (*entity.User, error)
+	Update(ctx context.Context, user *entity.User) error
+	Delete(ctx context.Context, id valueobject.UserID) error
+	ExistsByEmail(ctx context.Context, email valueobject.Email) (bool, error)
+}
