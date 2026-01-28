f0756aa21b0fb0603453b9bd632943b088d587c4
Seigha Rufus <excellenceseigha@gmail.com>
2026-01-28 23:35:39 +0100

    feat: implement password hasher using bcrypt

---
+++ internal/infrastructure/security/password_hasher.go
@@ -0,0 +1,30 @@
+package security
+
+import "golang.org/x/crypto/bcrypt"
+
+type PasswordHasher interface {
+	// Hash generates a hash from plain text password
+	Hash(password string) (string, error)
+
+	// Compare compares plain text password with hash
+	Compare(hashedPassword, password string) error
+}
+
+type BcryptHasher struct {
+	cost int
+}
+
+func NewBcryptHasher(cost int) *BcryptHasher {
+	if cost < bcrypt.MinCost || cost > bcrypt.MaxCost {
+		cost = bcrypt.DefaultCost // 10
+	}
+
+	return &BcryptHasher{
+		cost: cost,
+	}
+}
+
+func (h *BcryptHasher) Compare(hashedPassword, password string) error {
+	// bcrypt.CompareHashAndPassword is constant-time
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
